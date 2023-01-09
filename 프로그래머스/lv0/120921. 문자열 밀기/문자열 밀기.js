963155152ef36a338f253a9ff3f42b994bb15a71
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-09 15:02:55 +0900

    Update 문자열 밀기.js

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -4,3 +4,23 @@ function solution(A, B) {
     // hello = 1번 배열에 있음으로 1번 민 것 return
     return (B+B).indexOf(A);
 }
+
+
+function solution2(A, B) {
+    if(A===B) return 0;
+
+    var answer = 0;
+    let arr = [...A];
+
+    for (let i = 0; i < arr.length; i++) {
+        // A의 마지막 문자를 tmp 삽입
+        let tmp = arr.splice(arr.length-1, 1)[0];
+        // arr 맨 앞에 A의 마지막 문자 삽입
+        arr.unshift(tmp);
+        // 밀었음으로 answer ++
+        answer++;
+        // 미는 도중 arr의 문자열과 B가 같을 시 함수 종료
+        if(arr.join("") === B) return answer;
+    }
+    return -1;
+}
