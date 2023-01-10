afffe3fac05fee157b9de400799f94388eaeaa26
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-10 17:13:05 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,7 +1,5 @@
 function solution(my_str, n) {
     var answer = [];
-    for (let i = 0; i < Math.ceil(my_str.length/n); i++) {
-        answer.push(my_str.slice(i*n,n*(i+1)));
-    }
+    for (let i = 0; i < my_str.length; i+=n) answer.push(my_str.slice(i, i+n)); 
     return answer;
 }
