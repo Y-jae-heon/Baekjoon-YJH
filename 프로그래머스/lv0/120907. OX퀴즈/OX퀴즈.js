7a0cc576081943ca4e0b6aa7f58109ff9fb47572
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-11 16:57:48 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,12 +1,8 @@
 function solution(quiz) {
     var answer = [];
-    quiz.map((a, i) => {
+    answer = quiz.map((a, i) => {
         const el = eval(a.split("="));
-        if(eval(el[0]) == Number(el[1])){
-            answer.push("O");
-        }else{
-            answer.push("X")
-        }
+        return eval(el[0]) == el[1] ? "O" : "X";
     })
     return answer;
 }
