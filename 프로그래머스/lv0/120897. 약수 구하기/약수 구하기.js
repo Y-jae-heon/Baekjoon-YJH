9201c6f672164ccbb4b8b324f3b03433b7746ace
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-11 18:19:06 +0900

    [level 0] Title: 약수 구하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,3 @@
-function solution(n) {
-    var i = 0;
-    var ans = [];
-    while (i <= n) {
-        if(n%i == 0){
-            ans.push(i);
-        }
-        i++;
-    }
-    return ans;
+const solution = (n) =>{
+    return Array(n).fill(0).map((v,i) => v+i+1).filter((v) => n%v===0);
 }
