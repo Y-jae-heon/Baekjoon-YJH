d2b0856d86dad221a61efd5ab68d23f23109a184
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-12 17:09:53 +0900

    [level 0] Title: k의 개수, Time: 9.65 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,7 +1,7 @@
 function solution(i, j, k) {
     var answer = 0;
     for (i; i <= j; i++) {
-        answer +=String(i).split("").filter(x => String(k) == x).length;
+        answer +=String(i).split(k).length - 1;
     }
     return answer;
 }
