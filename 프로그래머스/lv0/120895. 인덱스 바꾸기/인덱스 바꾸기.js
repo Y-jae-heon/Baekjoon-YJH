1da0e072f82b288c81892774b21cc02f9b199624
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-12 14:28:35 +0900

    Update 인덱스 바꾸기.js

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -4,3 +4,9 @@ function solution(my_string, num1, num2) {
     str[num2] = my_string[num1];
     return str.join("");
 }
+
+function solution2(my_string, num1, num2) {
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
+}
