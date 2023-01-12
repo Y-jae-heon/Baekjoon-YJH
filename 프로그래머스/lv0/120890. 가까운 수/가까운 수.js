16ffc7ccd09e789d50aa64cd658fb217d02bf695
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-12 16:11:24 +0900

    Update 가까운 수.js

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -3,3 +3,7 @@ function solution(arr, n) {
         return Math.abs(x-n) === Math.min(...arr.map(a => Math.abs(a-n)));
     }).sort((a,b) => a-b)[0];
 }
+
+function solution2(array, n) {
+    return array.reduce((a,c)=> Math.abs(a-n) < Math.abs(c-n) ? a : Math.abs(a-n) === Math.abs(c-n) ? Math.min(a, c) : c);
+}
