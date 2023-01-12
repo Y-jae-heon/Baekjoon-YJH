374c4bad9b999cbd6ea776b684ff161d4e9f7f2e
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-12 14:56:05 +0900

    Update 영어가 싫어요.js

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -3,3 +3,23 @@ function solution(numbers) {
     en.map((x,i) => numbers = numbers.split(x).join(i));
     return Number(numbers);
 }
+
+function solution2(n) {
+    return Number(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    .reduce((t,s,i) => {
+        console.log(t,s,i);
+        return t.replaceAll(s,i)
+    },n))
+}
+
+function solution3(numbers) {
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return obj[v];
+    });
+
+    return Number(num);
+}
