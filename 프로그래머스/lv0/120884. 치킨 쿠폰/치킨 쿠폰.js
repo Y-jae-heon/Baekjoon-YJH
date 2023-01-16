0ea36376fdc0cd8ffb32b214683175c2ca71268d
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-16 17:57:36 +0900

    Update 치킨 쿠폰.js

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -8,3 +8,15 @@ function solution(chicken) {
     }
     return Math.round(coupon);
 }
+
+function solution2(chicken) {
+    let answer = 0;
+    let coupon = chicken
+
+    while(coupon >= 10){
+        answer = answer + parseInt(coupon/10);
+        coupon = parseInt(coupon/10)+ coupon%10;
+    }
+
+    return answer;
+}
