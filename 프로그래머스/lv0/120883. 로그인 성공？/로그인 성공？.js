299952990951560b9944193105251b0f5def1850
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-16 18:08:33 +0900

    Update 로그인 성공？.js

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -7,3 +7,10 @@ function solution(id_pw, db) {
         return x.indexOf(id_pw[1]) != -1;
     }).length === 0 ? "wrong pw" : "login"
 }
+
+function solution2(id_pw, db) {
+  const [id, pw] = id_pw;
+  console.log(id);
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
+}
