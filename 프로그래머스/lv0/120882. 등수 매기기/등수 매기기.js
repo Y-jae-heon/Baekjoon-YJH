2c7a5b154bacaecb3dab8d0378a14801d7d2dec6
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-17 23:29:12 +0900

    Update 등수 매기기.js

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,5 +1,6 @@
 function solution(score) {
     var avg = score.map(x => (x[0]+x[1])/2);
+    // 기존의 avg를 변경하지않으려면 slice() 를 붙여주어야함
     var sorted = avg.slice().sort((a,b) => b-a);
    
     return avg.map(x => sorted.indexOf(x)+1);
