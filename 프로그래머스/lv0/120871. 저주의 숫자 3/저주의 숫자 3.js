a208740fd1baaf84cbea9f6cd0d61ed80e7a2416
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-19 21:36:52 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,10 +1,5 @@
 function solution(n) {
-    var answer = 0;
-    var i = 0;
-    while (i < n) {
-        if((answer).toString().indexOf('3') == -1) {i++; answer++};
-        if((answer).toString().indexOf('3') != -1) answer++;
-        if(answer%3 === 0) answer++;
-    }
-    return answer;
+  return [...Array(n * 3)]
+    .map((_, i) => i + 1)
+    .filter((num) => num % 3 !== 0 && !num.toString().includes("3"))[n - 1];
 }
