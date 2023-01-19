6e4f3dc0a40101573ff502f56f168849781eba8c
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-19 23:11:06 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,12 +1,3 @@
 function solution(sides) {
-    var answer = 0;
-    const [n1, n2] = sides;
-    for (let e = Math.max(n1,n2)+1; e < n1+n2; e++) {
-        answer++;
-    }
-    for (let e = Math.max(n1,n2)-Math.min(n1,n2)+1; e <= Math.max(n1,n2); e++) {
-        answer++;
-    }
-    
-    return answer;
+    return Math.min(...sides)*2-1
 }
