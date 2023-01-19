ef71cfe471aee23b201fa5b8d2d74f19d8815cf8
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-19 21:57:43 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,8 +1,3 @@
-function solution(spell, dic) {
-    var test = dic.reduce((a,b) => {
-        a[b] = spell.map(x =>  b.includes(x)).filter(x => x).length;
-        return a;
-    }, {});
-
-    return Object.values(test).indexOf(spell.length) != -1 ? 1 : 2;
+function solution(p, d) {
+    return d.some(s => p.sort().toString() == [...s].sort().toString()) ? 1 : 2;
 }
