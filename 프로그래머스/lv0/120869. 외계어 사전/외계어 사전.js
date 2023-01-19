70c91dc44ef8dda8df860f94002f44bc3d93d8f2
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-19 22:47:56 +0900

    [level 0] Title: 외계어 사전, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,13 +1,4 @@
 function solution(spell, dic) {
-    // some = new Set == new Set
-    return dic.some(s => spell.sort().toString() == [...s].sort().toString()) ? 1 : 2;
-}
-// 위 예제는 spell의 원소로만 이루어진 단어만 확인 가능
-// 그 외 다른 단어가 함께 들어가있으면 2를 return
-
-// 아래 예제는 spell의 원소로만 이루어진 단어가 하나씩 있는지만 체크
-// 그 외 다른 단어가 함께 들어가있어도 1을 return
-function solution2(spell, dic) {
     var arr = dic.reduce((a,b) => {
         a[b] = spell.map(x => {
             // spell의 한 원소가 두번이상 들어갔는지 indexOf == lastIndexOf
@@ -17,6 +8,6 @@ function solution2(spell, dic) {
         }).filter(x => x).length;
         return a;
     }, {});
-
-    return Object.values(arr).indexOf(spell.length) != -1 ? 1 : 2;
+    
+    return Object.values(arr).includes(spell.length) ? 1 : 2;
 }
