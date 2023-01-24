478ea8672b311bc8b533318794de94f89f3016e9
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-24 23:51:34 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,7 +1,4 @@
-function solution(numbers) {
-    const [a,b] = [numbers.splice(numbers.indexOf(Math.max(...numbers)), 1), numbers.splice(numbers.indexOf(Math.max(...numbers)), 1)];
-    const [a2,b2] = [numbers.splice(numbers.indexOf(Math.min(...numbers)), 1), numbers.splice(numbers.indexOf(Math.min(...numbers)), 1)];
-    if(a2.length == 0 && b2.length == 0) return a*b;
-    if(a*b > a2*b2) return a*b;
-    return a2*b2;
+function solution(num) {
+    num.sort((a,b) => a-b);
+    return Math.max(num[0]*num[1], num[num.length-1]*num[num.length-2]);
 }
