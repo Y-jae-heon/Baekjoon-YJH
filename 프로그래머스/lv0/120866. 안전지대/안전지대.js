81897f661d64a1107a49be8bfd96bf3f073aa595
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-24 22:09:24 +0900

    [level 0] Title: 안전지대, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,26 +1,13 @@
 function solution(board) {
-        let ans = 0;
-        const bod = JSON.parse(JSON.stringify(board));
 
-        for (let i = 0; i < board.length; i++) {
-            const el = bod[i];
-            for (let e = 0; e < el.length; e++) {
-                const el2 = el[e];
-                if(el2 >= 1) { 
-                    board[i][e] = false;
-                    if(e-1 >= 0) board[i][e-1] = false;
-                    if(e+1 < el.length) board[i][e+1] = false;
-                    if(i-1 >= 0) board[i-1][e] = false;
-                    if(i-1 >= 0 && e-1 >= 0) board[i-1][e-1] = false;
-                    if(i-1 >= 0) board[i-1][e+1] = false;
-                    if(i+1 < board.length) board[i+1][e] = false;
-                    if(i+1 < board.length && e-1 >= 0) board[i+1][e-1] = false;
-                    if(i+1 < board.length && e+1 < el.length) board[i+1][e+1] = false;
-                }
-            }
-        }
-        board.map(x => x.map(e => {
-            if(e !== false) ans++;
-        }))
-        return ans;
-    };
+    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
+    let ans = 0;
+
+    board.forEach((row, y, self) => row.forEach((it, x) => {
+        if (it === 1) return false;
+        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
+               ? false : ans++;
+    }));
+
+    return ans;
+}
