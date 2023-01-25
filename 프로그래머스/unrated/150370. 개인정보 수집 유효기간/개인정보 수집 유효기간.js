c24c4af2316e0b9b1a590f8cc27ecd488a85eaeb
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2023-01-25 15:32:06 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.73 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,36 +1,17 @@
 function solution(today, terms, privacies) {
-    var answer = [];
-    const reg = /\D+/;
-    const regA = /[^a-zA-Z]/g;
-
-    const period = terms.reduce((acc,x) => {
-        const month = Number(x.split(reg).join(""));
-        const type = x.split(regA).join("");
-        acc[type] = month;
-        return acc;
-    },{});
-
-    const collect = privacies.reduce((acc, x) => {
-        const type = x.split(regA).join("");
-        const date = x.split(reg).splice(0,3).join(".");
-        let month = period[type] + new Date(date).getMonth()+1;
-        let cnt = 0;
-        if(month > 12) while (month > 12) month -= 12, cnt++;
-        let day = new Date(date).getDate()-1;
-        if(day === 0)day = 28, month -=1;
-        const year = new Date(date).getFullYear() + cnt;
-
-        acc.push(`${year}.${month < 10 ? `0${month}` : month}.${day < 10 ? `0${day}` : day}`);
-        return acc;
-    },[]);
-
-    for (let i = 0; i < collect.length; i++) {
-        const el = collect[i];
-        if(el < today){
-            answer.push(i+1);
-        }
-        
-    };
-
-    return answer;
+  var answer = [];
+  var [year, month, date] = today.split(".").map(Number);
+  var todates = year * 12 * 28 + month * 28 + date;
+  var t = {};
+  terms.forEach((e) => {
+    let [a, b] = e.split(" ");
+    t[a] = Number(b);
+  });
+  privacies.forEach((e, i) => {
+    var [day, term] = e.split(" ");
+    day = day.split(".").map(Number);
+    var dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + t[term] * 28;
+    if (dates <= todates) answer.push(i + 1);
+  });
+  return answer;
 }
